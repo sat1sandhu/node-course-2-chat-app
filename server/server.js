4a1f65a019a94b6e4e098d536fe164f4c5f91b67
sat1sandhu <sat1sandhu@yahoo.com>
2017-09-08 18:24:53 +1000

    Rectify server/server.js file 2

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ const port = process.env.PORT || 3000;
 
 app.use (express.static(publicPath));
 
-app.listen (PORT, () => {
+app.listen (port, () => {
     console.log (`Server is up on port ${port}`);
 });
 
