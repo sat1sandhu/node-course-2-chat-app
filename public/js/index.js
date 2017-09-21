aa45d03e4e8c53160852a6c6fd73ac31013f87b3
sat1sandhu <sat1sandhu@yahoo.com>
2017-09-21 23:28:40 +1000

    Add form for messages and show incoming messages in browser

--- public/js/index.js
+++ public/js/index.js
@@ -2,10 +2,6 @@ var socket = io();
 
 socket.on ('connect', () => {
     console.log ("Connected to server");
-
-    // socket.on ('newUser', function (message) {
-    //       console.log('newUser', message);
-    // });
 });
 
 socket.on ('disconnect', () => {
@@ -14,8 +10,18 @@ socket.on ('disconnect', () => {
 
 socket.on ('newMessage', function (message) {
     console.log ('New message:', message);
+    var li = jQuery('<li></li>');
+    li.text(`${message.from}: ${message.text}`);
+    jQuery('#messages').append(li);
 });
 
-socket.on ('newUserJoined', function (message) {
-    console.log ('newUserJoined', message);
+
+jQuery('#message-form').on ('submit', function (e) {
+    e.preventDefault();
+    socket.emit('createMessage', {
+          from: 'User',
+          text: jQuery('[name=message]').val()
+    }, function () {
+
+    });
 });
--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const path      = require ('path');
 const http      = require ('http');
 const express   = require ('express');
 const socketIO  = require ('socket.io');
-const {generatemessage} = require ('./utils/message');
+const {generateMessage} = require ('./utils/message');
 
 /// Serving static files in Express
 const publicPath = path.join (__dirname, '../public');
@@ -28,9 +28,10 @@ io.on ('connection', (socket) => {
     socket.broadcast.emit ('newMessage', generateMessage('Admin', "New user joined"));
 
 
-    socket.on ('createMessage', (message) => {
+    socket.on ('createMessage', (message, callback) => {
         console.log ("Received Message: ", message);
         io.emit ('newMessage', generateMessage(message.from, message.text));
+        callback('This is from the server');
         // socket.broadcast.emit ('newMessage', {
         //       from: message.from,
         //       text: message.text,
